3394d16315d9f774070516f47883e8e20afddc62
rozer <shubhamsinha07084@gmail.com>
2024-06-02 23:19:45 +0530

    Navbar & constants

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { headerLogo } from '../assets/images'
+import { hamburger } from '../assets/icons'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='padding-x py-8 absolute w-full z-10'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+          <img src={headerLogo} alt='logo' className='w-24 h-8 ' />
+        </a>
+        <ul className='flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item) =>
+            <li key={item.label}>
+              <a href={item.href}
+                className='font-montserrat text-md text-slate-gray hover:text-red-600 hover:border-b-[1px] hover:border-b-red-600'
+              >
+                {item.label}
+              </a>
+            </li>
+          )}
+        </ul>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger'
+        className='w-4 h-4'  
+        />
+        </div>
+      </nav>
+    </header>
   )
 }
 
