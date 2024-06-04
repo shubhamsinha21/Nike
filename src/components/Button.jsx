9eced7545921a6d18f4cfc8d44c0686a5c044bcf
rozer <shubhamsinha07084@gmail.com>
2024-06-04 14:28:52 +0530

    SuperQuality  Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,19 @@
-const Button = ({label, iconUrl}) => {
+const Button = ({ label, iconUrl }) => {
   return (
     <button className='border flex justify-center 
     items-center gap-2 px-7 py-4 
-    font-montserrat font-semibold text-md bg-coral-red border-coral-red text-white rounded-full'>
-       {label}
-       <img src={iconUrl} alt='arrow right icon' className='size-5 ml-2 rounded-full' />
+    font-montserrat font-semibold text-md bg-coral-red
+     border-coral-red text-white rounded-full w-fit
+     hover:bg-orange-600 shadow-xl group
+     '>
+      {label}
+
+      {iconUrl &&
+        <img src={iconUrl} alt='arrow right icon'
+          className='size-5 ml-2 rounded-full group-hover:translate-x-2 
+          transition duration-300' />}
     </button>
+
   )
 }
 
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,43 @@
+import Button from "../components/Button";
+import {shoe8} from "../assets/images"
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-around items-center
+    max-container gap-10 w-full max-lg:flex-col "
+    >
+      <div className="flex flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You {""}
+          <span className="text-coral-red">Super</span> {""}
+          <span className="text-coral-red">Quality</span> {""}
+          Shoes
+        </h2>
+        <p
+          className="font-montserrat text-slate-gray 
+        leading-8 mt-6 mb-14 lg:max-w-lg"
+        >
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elevance.
+        </p>
+        <p className="mt-6 info-text lg:max-w-lg">
+          Our dedication to detail and excellence ensures your satisfaction.
+        </p>
+        <div className="mt-10">
+          <Button label="View Details" />
+        </div>
+      </div>
 
-export default SuperQuality
+      <div className="flex justify-center items-center drop-shadow-lg cursor-pointer hover:scale-110 transition duration-300 ">
+        <img src={shoe8} alt="shoe8"
+        className='w-[530px] h-[510px] object-contain'
+        />
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
